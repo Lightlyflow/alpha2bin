585cde6f622f1d5ff7cd879d203cde31f7aff872
Lightlyflow <tongqu777@gmail.com>
2022-06-12 22:50:28 -0500

    Added quiet mode

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ use std::error::Error;
 pub struct Config {
     pub query_string: String,
     pub bin_to_alpha: bool,
+    pub quiet: bool,
 }
 
 impl Config {
@@ -11,39 +12,47 @@ impl Config {
             return Err("Not enough arguments!");
         }
 
+        // Default values
         let mut query_string = "".to_string();
         let mut bin_to_alpha = false;
+        let mut quiet = false;
+
         // Parse cli args
         for (i, arg) in args.iter().enumerate() {
             if i == 0 { continue; }
             else if arg == "-r" { bin_to_alpha = true; }
+            else if arg == "-q" { quiet = true; }
             else { query_string = arg.clone(); }
         }
 
         Ok(Config {
             query_string,
             bin_to_alpha,
+            quiet,
         })
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let mut output = String::new();
+    let mut mode = "None".to_string();
 
+    // Get output
     if config.bin_to_alpha {
+        mode = "binary representation to string".to_string();
         output = bin_to_alpha(&config.query_string)?;
     } else {
+        mode = "string to binary".to_string();
         output = str_to_bin(&config.query_string)?;
     }
 
-    println!("{}", output);
+    if !config.quiet { println!("Mode: {mode}"); }
+    println!("{output}");
 
     Ok(())
 }
 
 fn str_to_bin(alpha_string: &String) -> Result<String, Box<dyn Error>> {
-    println!("Mode: string to binary");
-
     let mut output: String = "".to_string();
 
     for char in alpha_string.to_string().into_bytes() {
@@ -54,8 +63,6 @@ fn str_to_bin(alpha_string: &String) -> Result<String, Box<dyn Error>> {
 }
 
 fn bin_to_alpha(bin_string: &String) -> Result<String, Box<dyn Error>> {
-    println!("Mode: binary representation to string");
-
     let vec_u8 = (0..bin_string.len())
                 .step_by(9)
                 .map(|i| u8::from_str_radix(&bin_string[i..i + 8], 2))
